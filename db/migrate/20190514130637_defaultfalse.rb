44facfb034d5550d3adf7b1afa0801a694310670
Julian Hermann <julian.hermann89@gmail.com>
2019-05-15 11:27:52 +0200

    contr

---
+++ db/migrate/20190514130637_defaultfalse.rb
@@ -0,0 +1,10 @@
+class Defaultfalse < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default(
+      :tasks,
+      :completed,
+      from: nil,
+      to: false
+    )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_104531) do
+ActiveRecord::Schema.define(version: 2019_05_14_130637) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
